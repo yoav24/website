11f7599a926fc61d3cf7994d45a623a2cd0c4464
yoav24 <yoavgil3@gmail.com>
2022-05-12 18:04:44 +0300

    first commit

---
+++ quiz/quiz.js
@@ -0,0 +1,117 @@
+document.getElementById("answer1").onclick = function basketball() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer2").onclick = function basketball2() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer3").onclick = function basketball3() {
+  document.getElementById("question1").innerHTML = ``;
+  document.getElementById("to").innerText = "?איזו קבוצה זכתה בגביע המדינה";
+  document.getElementById("answer5").innerText = "בני הרצליה";
+  document.getElementById("answer6").innerText = "הפועל תל אביב";
+  document.getElementById("answer7").innerText = "מכבי תל אביב";
+  document.getElementById("answer8").innerText = "הפועל חולון";
+};
+document.getElementById("answer4").onclick = function basketball4() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer5").onclick = function basketball5() {
+  document.getElementById("question2").innerHTML = ``;
+  document.getElementById("to").innerText = "?עכשיו NBAאיזה משחק ב";
+  document.getElementById("answer9").innerText = "ג'יימס יאנג";
+  document.getElementById("answer10").innerText = "סקוטי ולבקין";
+  document.getElementById("answer11").innerText = "דני אבדיה";
+  document.getElementById("answer12").innerText = "ים מדר";
+};
+document.getElementById("answer6").onclick = function basketball6() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer7").onclick = function basketball7() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer8").onclick = function basketball8() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer9").onclick = function basketball9() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer10").onclick = function basketball10() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer11").onclick = function basketball11() {
+  document.getElementById("question3").innerHTML = ``;
+  document.getElementById("to").innerText =
+    "?מי הקבוצה שלקחה הכי הרבה אליפויות בליגת העל";
+  document.getElementById("answer13").innerText = "הפועל גליל עליון";
+  document.getElementById("answer14").innerText = "מכבי תל אביב";
+  document.getElementById("answer15").innerText = "מכבי חיפה";
+  document.getElementById("answer16").innerText = "הפועל ירושלים";
+};
+document.getElementById("answer12").onclick = function basketball12() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer13").onclick = function basketball13() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer14").onclick = function basketball14() {
+  document.getElementById("question4").innerHTML = ``;
+  document.getElementById("to").innerText = "?NBAמי האלופה הנוכחית ב";
+  document.getElementById("answer17").innerText = "Golden State Warriors";
+  document.getElementById("answer18").innerText = "Milwaukee Bucks";
+  document.getElementById("answer19").innerText = "Los Angeles Lakers";
+  document.getElementById("answer20").innerText = "Phoenix Suns";
+};
+document.getElementById("answer15").onclick = function basketball15() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer16").onclick = function basketball16() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer17").onclick = function basketball17() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer18").onclick = function basketball18() {
+  document.getElementById("to").innerText =
+    "?Los Angeles Lakersמי שיחק הכי הרבה זמן ב";
+  document.getElementById("question5").innerHTML = ``;
+  document.getElementById("answer21").innerText = "LeBron James";
+  document.getElementById("answer22").innerText = "Magic Johnson";
+  document.getElementById("answer23").innerText = "Shaquille O'Neal";
+  document.getElementById("answer24").innerText = "Kobe Bryant";
+};
+document.getElementById("answer19").onclick = function basketball19() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer20").onclick = function basketball20() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer21").onclick = function basketball21() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer22").onclick = function basketball22() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer23").onclick = function basketball23() {
+  document.getElementById("final1").innerText = "תשובה לא נכונה";
+};
+document.getElementById("answer24").onclick = function basketball24() {
+  document.getElementById("to").innerText =
+    "?allstarבתור קפטן במשחק ה LeBron James מה המאזן של";
+  document.getElementById("question6").innerHTML = ``;
+  document.getElementById("answer25").innerText = "3-4";
+  document.getElementById("answer26").innerText = "6-1";
+  document.getElementById("answer27").innerText = "5-2";
+  document.getElementById("answer28").innerText = "5-0";
+};
+document.getElementById("answer25").onclick = function basketball25() {
+    document.getElementById("final1").innerText = "תשובה לא נכונה";
+  };
+  document.getElementById("answer26").onclick = function basketball26() {
+    document.getElementById("final1").innerText = "תשובה לא נכונה";
+  };
+  document.getElementById("answer27").onclick = function basketball27() {
+    document.getElementById("final1").innerText = "תשובה לא נכונה";
+  };
+  document.getElementById("answer28").onclick = function basketball28() {
+    document.getElementById("final1").innerText = "תשובה נכונה";
+  };
